7fbd611d4e6fb286f3f4d8ce4674e57d3dd3896e
Al Min <albertmin@Lilu.local>
2017-09-20 17:42:29 -0400

    authentication and error catching

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
 import firebase from 'firebase';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
 
   onButtonPress() {
     const { email, password } = this.state;
 
-    firebase.auth().signInWithEmailAndPassword(email, password);
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
   }
 
   render() {
@@ -32,6 +39,10 @@ class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
         
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
@@ -42,5 +53,13 @@ class LoginForm extends Component {
     );
   }
  }
+
+ const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+ };
 
  export default LoginForm;
