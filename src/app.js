ca5048c01e5fe0834febfa777fa9a58b66185ab3
Al Min <albertmin@Lilu.local>
2017-09-20 17:30:46 -0400

    password component and firefirebase

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyA-ZATSBpNpys6yv6GRQr2h-3UD3ZhavVk',
+    authDomain: 'reactnativeauth-4855b.firebaseapp.com',
+    databaseURL: 'https://reactnativeauth-4855b.firebaseio.com',
+    projectId: 'reactnativeauth-4855b',
+    storageBucket: 'reactnativeauth-4855b.appspot.com',
+    messagingSenderId: '501427989444'
+  });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>A Schmapp!</Text>
+        <LoginForm />
       </View>
     );
   }
